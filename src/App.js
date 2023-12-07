6abae2fdbaf1dbc7885cdde28edeab3bc0edf9bc
Thibaut <hertzogthibaut@gmail.com>
2023-12-07 18:03:15 +0100

    Ajout d'une route + Homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Route, Routes, Switch} from 'react-router-dom';
 
+import Menu from "./components/menu/Menu";
+import HomePage from "./page/HomePage";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-
+                <Menu/>
             </header>
             <body>
-            <h1>hello world</h1>
-            <Router>
-            </Router>
+            <div>
+                <Routes>
+                    <Route path={"/"} element={<HomePage/>} />
+                </Routes>
+            </div>
             </body>
         </div>
     );
