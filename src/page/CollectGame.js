cf333a20339854219228e28bd0d1e0d9707861e1
Lucas <Lucas@git.atlas-connect.fr>
2023-12-08 03:14:13 +0100

    General improvements & bug fixes

--- src/page/CollectGame.js
+++ src/page/CollectGame.js
@@ -5,6 +5,7 @@ export default CollectGame;
 
 let InGame = false;
 
+let objects = [];
 let trash;
 let recycler_bean;
 let score = 0;
@@ -12,27 +13,27 @@ let types = ["recyclable", "non_recyclable"];
 
 function CollectGame() {
     return (
-        <>
+        <div>
+            <h1>Score : <span id={"score"}>{score}</span></h1>
             <div onLoad={GameHandler()} className={"canvas"}>
                 <div>
-                    <button onClick={motion}>Start</button>
+                    <button onClick={Start}>Start</button>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
 function GameHandler() {
-    GameArea.init();
-    trash = new Object(50, 50, Math.floor(Math.random() *(0.8*window.innerWidth-150)), 20, types[Math.floor(Math.random() * 2)]);
-    recycler_bean = new Object(150, 40,(0.8*window.innerWidth-150)/2, window.innerHeight-40, "recycler_bean");
+    GameArea.init(window.innerWidth*0.5, window.innerHeight);
+    recycler_bean = new Object(150, 40,(GameArea.canvas.width-150)/2, window.innerHeight-40, "recycler_bean");
 }
 
 let GameArea = {
     canvas: document.createElement("canvas"),
-    init: function () {
-        this.canvas.width = window.innerWidth*0.8;
-        this.canvas.height = window.innerHeight;
+    init: function (canvas_width, canvas_height) {
+        this.canvas.width = canvas_width;
+        this.canvas.height = canvas_height;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -77,19 +78,37 @@ function Object(width, height, x, y, type) {
     }
 }
 
+function createTrash() {
+    trash = new Object(50, 50, Math.floor(Math.random() * (GameArea.canvas.width - 150)), 20, types[Math.floor(Math.random() * 2)]);
+    objects.push(trash);
+    motion(objects);
+}
+
 function updateGameArea() {
     GameArea.clear();
-    trash.newPos();
+
+    objects.forEach(function (object) {
+        object.newPos();
+        object.update();
+    }, this);
+
     recycler_bean.newPos();
-    trash.update();
     recycler_bean.update();
+
     keybordListener();
     checkDeath();
 }
 
-function motion() {
-    trash.speedY += 5;
+function Start() {
     InGame = true;
+    createTrash();
+    setInterval(createTrash, 5000);
+}
+
+function motion(objects) {
+    objects.forEach(function (object) {
+        object.speedY = 5;
+    }, this);
 }
 
 function keybordListener() {
@@ -110,8 +129,8 @@ function leftMove() {
 
 function rightMove() {
     recycler_bean.x += 10;
-    if (recycler_bean.x > window.innerWidth*0.8 - recycler_bean.width) {
-        recycler_bean.x = window.innerWidth*0.8 - recycler_bean.width;
+    if (recycler_bean.x > GameArea.canvas.width - recycler_bean.width) {
+        recycler_bean.x = GameArea.canvas.width - recycler_bean.width;
     }
 }
 
@@ -135,12 +154,22 @@ function Score(type, got) {
 }
 
 function checkDeath() {
-    if (trash.y > window.innerHeight) {
+    if (objects.length > 0) {
+        objects.forEach(function (object) {
+            checkTrash(object);
+        }, this);
+    }
+    document.getElementById("score").innerHTML = score;
+}
+
+function checkTrash(trash) {
+    if (trash.y > GameArea.canvas.height) {
         trash.y = 0;
         trash.speedY = 0;
         console.log(trash.type + "Reach position 0");
         score = Score(trash.type, false);
         console.log(score);
+        objects.splice(objects.indexOf(trash), 1);
     }
     if (trash.y+50 > recycler_bean.y && trash.y+50 < recycler_bean.y + recycler_bean.height && trash.x > recycler_bean.x && trash.x < recycler_bean.x + recycler_bean.width) {
         trash.y = 0;
@@ -148,5 +177,6 @@ function checkDeath() {
         console.log(trash.type + "Touched the recycler_bean");
         score = Score(trash.type, true);
         console.log(score);
+        objects.splice(objects.indexOf(trash), 1);
     }
 }
